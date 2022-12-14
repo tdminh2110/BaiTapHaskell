acbf40b5817edb78dd91f5451a881384107219bb
Tran Duc Minh <tdminh2110@yahoo.com>
2022-12-14 00:31:10 +0000

    Test

--- Tuan2/baitap.hs
+++ Tuan2/baitap.hs
@@ -1,1 +1,12 @@
-hello
+-- Question 1
+-- Add the type signatures for the functions below and then remove the comments and try to compile.
+-- (Use the types presented in the lecture.)
+
+--f1 :: Double -> Double -> Double -> Double
+f1 x y z = x ** (y/z)
+
+-- f2 x y z = sqrt (x/y - z)
+
+-- f3 x y = [x == True] ++ [y]
+
+-- f4 x y z = x == (y ++ z)
