e58ed5bcbb8cbef8d2f37ba9039570ec100d9696
Tran Duc Minh <tdminh2110@yahoo.com>
2022-12-14 01:14:22 +0000

    Question 1

--- Tuan2/baitap.hs
+++ Tuan2/baitap.hs
@@ -2,11 +2,15 @@
 -- Add the type signatures for the functions below and then remove the comments and try to compile.
 -- (Use the types presented in the lecture.)
 
---f1 :: Double -> Double -> Double -> Double
+f1 :: Double -> Double -> Double -> Double
 f1 x y z = x ** (y/z)
 
--- f2 x y z = sqrt (x/y - z)
+f2 :: Float -> Float -> Float -> Float
+f2 x y z = sqrt (x/y - z)
 
--- f3 x y = [x == True] ++ [y]
+f3 :: Bool -> Bool -> [Bool]
+f3 x y = [x == True] ++ [y]
 
--- f4 x y z = x == (y ++ z)
+f4 :: [Double] -> [Double] -> [Double] -> Bool
+f4 x y z = x == (y ++ z)
+
