45876e33bd11eca19ed099a678b95a6732420db8
Tran Duc Minh <tdminh2110@yahoo.com>
2022-12-25 09:24:41 +0000

    Question 5

--- Tuan3/baitap.hs
+++ Tuan3/baitap.hs
@@ -45,3 +45,18 @@ myDivision a b
       if a > b
          then show (a / b)
          else show (b / a)
+
+-- Question 5
+-- Write a function that takes in two numbers and calculates the sum of squares for the product and quotient
+-- of those numbers. Write the function such that you use a where block inside a let expression and a
+-- let expression inside a where block. 
+
+mySum :: Double -> Double -> Double
+mySum a b =
+   let
+      exp = exp1 + exp2      
+   in
+      exp
+   where
+      exp1 = (a * b) ** 2
+      exp2 = (a / b) ** 2
