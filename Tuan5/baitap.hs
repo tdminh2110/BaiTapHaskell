d1e59736744b862bce77cbb405507f99ba60a938
Tran Duc Minh <tdminh2110@yahoo.com>
2023-01-08 06:22:38 +0000

    Question 1

---
+++ Tuan5/baitap.hs
@@ -0,0 +1,9 @@
+-- Create a higher-order function that takes 3 parameters: A function and the two parameters that that function takes, and
+-- flips the order of the parameters.
+-- For example this: `(/) 6 2` returns `3`. But this: `flip' (/) 6 2` returns `0.3333333333`
+
+flip' :: (a -> a -> b) -> a -> a -> b
+flip' f x y = f y x
+
+main = do
+    print(flip' (/) 6 2)
