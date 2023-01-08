6a3be517476dea5cfe7efcacbad0c7752d2b12b4
Tran Duc Minh <tdminh2110@yahoo.com>
2023-01-08 07:05:25 +0000

    Question 2

--- Tuan5/baitap.hs
+++ Tuan5/baitap.hs
@@ -7,3 +7,12 @@ flip' f x y = f y x
 
 main = do
     print(flip' (/) 6 2)
+
+-- Create the `uncurry'` function that converts a curried function to a function on pairs. So this: `(+) 1 2` that returns `3` can be written as
+-- `uncurry' (+) (1,2)` (with the two different arguments inside a pair).
+
+uncurry' :: (a -> a -> a) -> (a,a) -> a
+uncurry' f (x,y) = f x y
+
+main = do
+    print(uncurry' (+) (1,2))
